771981ed7dd2902502f863165428a7231cbac578
Nassif Nassif <nassif.nassif@vzbv.de>
2024-07-13 12:42:57 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+const newurl = 'https://www.comunicazione.it/api/Login'
 
 function App() {
+  const onClick = async () => {
+    const myHeaders = new Headers()
+    myHeaders.append('Content-Type', 'application/json')
+
+    const raw = JSON.stringify({
+      username: 'manisco.andrea@gmail.com',
+      password: 'Aa12345!',
+    })
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    }
+
+    fetch(newurl, requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +33,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+
+        <button onClick={onClick}>keke</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
